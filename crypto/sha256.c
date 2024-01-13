34eb7fad9dfdc082eb606888eb3bd88de1f43cfd
oladele90 <oladelemiro07@gmail.com>
2024-01-13 23:05:13 +0000

    adds betty style

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,15 +1,25 @@
 #include "hblk_crypto.h"
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+/**
+ * sha256 - Computes the SHA-256 hash of a sequence of bytes
+ * @s: Sequence of bytes to be hashed
+ * @len: Number of bytes to hash in s
+ * @digest: Array to store the resulting hash
+ *
+ * Return: Pointer to the digest or NULL if digest is NULL
+ */
+
+uint8_t *sha256(int8_t const *s, size_t len,
+				uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-    SHA256_CTX hash;
-    
-    if (digest == NULL)
-        return NULL;
+	SHA256_CTX hash;
 
-    SHA256_Init(&hash);
-    SHA256_Update(&hash, s, len);
-    SHA256_Final(digest, &hash);
+	if (digest == NULL)
+		return (NULL);
 
-    return digest;
+	SHA256_Init(&hash);
+	SHA256_Update(&hash, s, len);
+	SHA256_Final(digest, &hash);
+
+	return (digest);
 }

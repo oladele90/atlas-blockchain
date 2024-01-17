5a62f143f10a2c5f3d539a491a43f396010dc17e
oladele90 <oladelemiro07@gmail.com>
2024-01-17 17:00:10 +0000

    adds context and frees at end

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -8,9 +8,12 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
     const EC_POINT *public_key;
     const EC_GROUP *group;
 
+    if (!key)
+        return (NULL);
     ctx = BN_CTX_new();
     public_key = EC_KEY_get0_public_key(key);
     group = EC_KEY_get0_group(key);
     EC_POINT_point2oct(group, public_key, POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, ctx);
+    BN_CTX_free(ctx);
     return (pub);
 }

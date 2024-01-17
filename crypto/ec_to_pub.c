bf61627dae97afaa8c9f2dbbfa8b56bbd5d41edd
oladele90 <oladelemiro07@gmail.com>
2024-01-17 16:46:08 +0000

    get public key

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -4,11 +4,13 @@
 
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
+    BN_CTX *ctx;
     const EC_POINT *public_key;
     const EC_GROUP *group;
 
+    ctx = BN_CTX_new();
     public_key = EC_KEY_get0_public_key(key);
     group = EC_KEY_get0_group(key);
-    EC_POINT_point2oct(group, public_key, POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
+    EC_POINT_point2oct(group, public_key, POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, ctx);
     return (pub);
 }

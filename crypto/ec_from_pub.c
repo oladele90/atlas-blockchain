f528bc85fd5d7c29d683eb15f918f80f4e85f46c
oladele90 <oladelemiro07@gmail.com>
2024-01-17 18:09:47 +0000

    from buffer to key

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -14,6 +14,8 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	const EC_POINT *p;
 	const EC_GROUP *group;
 
+	if (!pub)
+		return (NULL);
 	ctx = BN_CTX_new();
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	group = EC_KEY_get0_group(key);
@@ -21,5 +23,6 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	EC_POINT_oct2point(group, (EC_POINT *)p, pub, EC_PUB_LEN, ctx);
 	EC_KEY_set_public_key(key, p);
 	BN_CTX_free(ctx);
+	EC_POINT_clear_free((EC_POINT *)p);
 	return (key);
 }

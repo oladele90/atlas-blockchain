dea5f8adb55772b9e7dfd09fe17fe37bd4f23eff
oladele90 <oladelemiro07@gmail.com>
2024-01-17 17:46:58 +0000

    take pub key from buffer to EC_KEY

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,25 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - creates an EC_KEY structure given a public key
+ * @pub: pointer to the public key buffer
+ *
+ * Return: a pointer to the created EC_KEY structure or NULL on failure
+ */
+
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	BN_CTX *ctx;
+	EC_KEY *key;
+	const EC_POINT *p;
+	const EC_GROUP *group;
+
+	ctx = BN_CTX_new();
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	group = EC_KEY_get0_group(key);
+	p = EC_POINT_new(group);
+	EC_POINT_oct2point(group, (EC_POINT *)p, pub, EC_PUB_LEN, ctx);
+	EC_KEY_set_public_key(key, p);
+	BN_CTX_free(ctx);
+	return (key);
+}

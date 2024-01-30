26d5d84c84c4a44e597a4a763dcaa1255055fa9e
oladele90 <oladelemiro07@gmail.com>
2024-01-30 18:30:48 +0000

    still have conditional jump

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -5,31 +5,41 @@
  *
  * Return: A pointer to the newly created blockchain
  */
-
 blockchain_t *blockchain_create(void)
 {
 	blockchain_t *new_chain = malloc(sizeof(blockchain_t));
 	block_t *new_block = malloc(sizeof(block_t));
-	block_info_t *new_info = malloc(sizeof(block_info_t));
-	block_data_t *new_data = malloc(sizeof(block_data_t));
 
-	memcpy(new_data->buffer, "Holberton School", strlen("Holberton School") + 1);
-	new_data->len = strlen((const char *)new_data->buffer);
-	new_info->index = 0;
-	new_info->difficulty = 0;
-	new_info->timestamp = 1537578000;
-	new_info->nonce = 0;
-	memset(new_info->prev_hash, 0, SHA256_DIGEST_LENGTH);
+	if (!new_block || !new_chain)
+	{
+		free(new_chain);
+		return (NULL);
+	}
+
+	new_chain->chain = llist_create(MT_SUPPORT_FALSE);
+	if (!new_chain->chain)
+	{
+		free(new_block);
+		free(new_chain);
+		return (NULL);
+	}
+
+	memcpy(new_block->data.buffer, "Holberton School",
+			strlen("Holberton School") + 1);
+	new_block->data.len = strlen((const char *)new_block->data.buffer);
+	new_block->info.index = 0;
+	new_block->info.difficulty = 0;
+	new_block->info.timestamp = 1537578000;
+	new_block->info.nonce = 0;
+	memset(new_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
 	memcpy(new_block->hash, "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d"
 							"\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09"
 							"\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51"
 							"\x58\x03", SHA256_DIGEST_LENGTH);
-	memcpy(&new_block->info, new_info, sizeof(block_info_t));
-	memcpy(&new_block->data, new_data, sizeof(block_data_t));
-	new_chain->chain = llist_create(MT_SUPPORT_FALSE);
-	llist_add_node(new_chain->chain, new_block, ADD_NODE_REAR);
+
+	llist_add_node(new_chain->chain, new_block, ADD_NODE_FRONT);
+	if (!llist_get_head(new_chain->chain))
+		printf("\n\n\n\n\nyou five me??\n\n\n\n\n");
 
 	return (new_chain);
-
-
 }

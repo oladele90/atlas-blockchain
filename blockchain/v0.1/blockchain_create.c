bf6a02cb3aecb8ffd4e23db313ada8ad655e3ab4
oladele90 <oladelemiro07@gmail.com>
2024-01-31 16:34:36 +0000

    take out members that = '0' because of calloc call

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -26,11 +26,7 @@ blockchain_t *blockchain_create(void)
 	memcpy(new_block->data.buffer, "Holberton School",
 			strlen("Holberton School") + 1);
 	new_block->data.len = strlen((const char *)new_block->data.buffer);
-	new_block->info.index = 0;
-	new_block->info.difficulty = 0;
 	new_block->info.timestamp = 1537578000;
-	new_block->info.nonce = 0;
-	memset(new_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
 	memcpy(new_block->hash, "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d"
 							"\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09"
 							"\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51"

34a06daf7ef16112456bd026d1c7ac05a4f9d52a
oladele90 <oladelemiro07@gmail.com>
2024-02-05 17:09:45 +0000

    hash

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -7,7 +7,7 @@ uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]
 
     SHA256_Init(&c);
     SHA256_Update(&c, (uint8_t const *)&(block->info), SHA256_DIGEST_LENGTH);
-	SHA256_Update(&c, (uint8_t const *)&(block->data.buffer), SHA256_DIGEST_LENGTH);
+	SHA256_Update(&c, &(block->data.buffer), SHA256_DIGEST_LENGTH);
 
     SHA256_Final(hash_buf, &c);
     return (hash_buf);

96ffe19e872875e3739e79196adcd20ae961eda0
oladele90 <oladelemiro07@gmail.com>
2024-02-12 17:36:13 +0000

    new sandboxes suck

--- blockchain/v0.1/blockchain_deserialize.c
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -1,43 +1,51 @@
 #include "blockchain.h"
 
+/**
+ * write_blocks - Deserialize blocks from a file and add them to a blockchain
+ * @size: Number of blocks to deserialize
+ * @fd: File descriptor of the file containing serialized blocks
+ * @chain: Pointer to the blockchain where blocks will be added
+ *
+ * Return: Pointer to the updated blockchain
+ */
 
 blockchain_t *write_blocks(uint32_t size, FILE *fd, blockchain_t *chain)
 {
-    block_t *block;
-    uint32_t i;
+	block_t *block;
+	uint32_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        block = calloc(1, sizeof(block_t));
-        fread(&block->info, sizeof(block_info_t), 1, fd);
-        fread(&(block->data.len), sizeof(uint8_t), 4, fd);
-        fread(block->data.buffer, sizeof(uint8_t), block->data.len, fd);
-        fread(block->hash, sizeof(uint8_t), SHA256_DIGEST_LENGTH, fd);
-        llist_add_node(chain->chain, block, ADD_NODE_REAR);
-    }
+	for (i = 0; i < size; i++)
+	{
+		block = calloc(1, sizeof(block_t));
+		fread(&block->info, sizeof(block_info_t), 1, fd);
+		fread(&(block->data.len), sizeof(uint8_t), 4, fd);
+		fread(block->data.buffer, sizeof(uint8_t), block->data.len, fd);
+		fread(block->hash, sizeof(uint8_t), SHA256_DIGEST_LENGTH, fd);
+		llist_add_node(chain->chain, block, ADD_NODE_REAR);
+	}
 
-    return (chain);
+	return (chain);
 
 }
 
 blockchain_t *blockchain_deserialize(char const *path)
 {
-    FILE *fd = fopen(path, "r");
-    char buf[4] = {0};
-    uint8_t end;
-    uint32_t size;
-    blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
+	FILE *fd = fopen(path, "r");
+	char buf[4] = {0};
+	uint8_t end;
+	uint32_t size;
+	blockchain_t *blockchain = calloc(1, sizeof(blockchain_t));
 
 
-    if (!fd)
-        return (NULL);
-    fread(buf, sizeof(uint8_t), 4, fd);
-    fread(buf, sizeof(uint8_t), 3, fd);
-    fread(&end, sizeof(uint8_t), 1, fd);
-    fread(&size, sizeof(uint32_t), 1, fd);
-    blockchain->chain = llist_create(MT_SUPPORT_FALSE);
-    blockchain = write_blocks(size, fd, blockchain);
-    fclose(fd);
+	if (!fd)
+		return (NULL);
+	fread(buf, sizeof(uint8_t), 4, fd);
+	fread(buf, sizeof(uint8_t), 3, fd);
+	fread(&end, sizeof(uint8_t), 1, fd);
+	fread(&size, sizeof(uint32_t), 1, fd);
+	blockchain->chain = llist_create(MT_SUPPORT_FALSE);
+	blockchain = write_blocks(size, fd, blockchain);
+	fclose(fd);
 
-    return (blockchain);
+	return (blockchain);
 }

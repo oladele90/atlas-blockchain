e88a4413f80708f2f2b5d6a3315e52582fd0c33c
oladele90 <oladelemiro07@gmail.com>
2024-02-12 18:31:37 +0000

    sandboxes are dumbA

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -11,15 +11,15 @@
 
 int writer(llist_node_t node, unsigned int idx, void *arg)
 {
-    FILE *fd = (FILE *)arg;
-    block_t *block = node;
+	FILE *fd = (FILE *)arg;
+	block_t *block = node;
 
-    (void)(idx);
-    fwrite(&block->info, sizeof(block->info), 1, fd);
-    fwrite(&block->data.len, sizeof(block->data.len), 1, fd);
+	(void)(idx);
+	fwrite(&block->info, sizeof(block->info), 1, fd);
+	fwrite(&block->data.len, sizeof(block->data.len), 1, fd);
 	fwrite(&block->data.buffer, block->data.len, 1, fd);
 	fwrite(&block->hash, sizeof(block->hash), 1, fd);
-    return (0);
+	return (0);
 
 }
 
@@ -33,16 +33,16 @@ int writer(llist_node_t node, unsigned int idx, void *arg)
 
 int blockchain_serialize(blockchain_t const *blockchain, char const *path)
 {
-    FILE *fd;
-    header_t new_header;
+	FILE *fd;
+	header_t new_header;
 
-    fd = fopen(path, "w");
-    memcpy(&new_header.magic, "HBLK", 4);
-    memcpy(new_header.version, "0.1", 3);
-    new_header.endian = _get_endianness();
-    new_header.blocks = (uint32_t)llist_size(blockchain->chain);
-    fwrite(&new_header, sizeof(new_header), 1, fd);
-    llist_for_each(blockchain->chain, writer, fd);
-    fclose(fd);
-    return (1);
+	fd = fopen(path, "w");
+	memcpy(&new_header.magic, "HBLK", 4);
+	memcpy(new_header.version, "0.1", 3);
+	new_header.endian = _get_endianness();
+	new_header.blocks = (uint32_t)llist_size(blockchain->chain);
+	fwrite(&new_header, sizeof(new_header), 1, fd);
+	llist_for_each(blockchain->chain, writer, fd);
+	fclose(fd);
+	return (1);
 }

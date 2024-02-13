8014166301df0dd33734fe6622c28d5d563fa428
oladele90 <oladelemiro07@gmail.com>
2024-02-13 19:18:34 +0000

    difficulty matches

---
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -0,0 +1,30 @@
+#include "blockchain.h"
+
+/**
+ * hash_matches_difficulty - Check if a hash meets a certain difficulty level.
+ * @hash: The hash to check.
+ * @difficulty: The difficulty level to match.
+ *
+ * Return: 1 if the hash meets the difficulty level, 0 otherwise.
+ */
+
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty)
+{
+	int i, j;
+	uint32_t count = 0;
+	uint8_t bit;
+
+	for (i = 0; i <= SHA256_DIGEST_LENGTH; i++)
+	{
+		for (j = 7; j >= 0; j--)
+		{
+			bit = (hash[i] >> j);
+			if (bit == 0)
+				count++;
+			else
+				return (count >= difficulty ? 1 : 0);
+		}
+	}
+	
+	return (count >= difficulty ? 1 : 0);
+}

33711d9e842c78b1d36072fa402ecb21e82ec6f2
root <root@LAPTOP-FFBJSM4C>
2024-03-03 08:55:15 -0600

    updates header and makes return type == pointer

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -1,16 +1,16 @@
 #include "transaction.h"
 
 unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
-                                        uint8_t tx_id[SHA256_DIGEST_LENGTH],
-                                        tx_out_t const *out)
+										uint8_t tx_id[SHA256_DIGEST_LENGTH],
+										tx_out_t const *out)
 {
-    unspent_tx_out_t new_u_tx;
+	unspent_tx_out_t *new_u_tx;
 
-    if (!tx_id || !block_hash || !out)
-        return (NULL);
-    new_u_tx = calloc(1, sizeof(unspent_tx_out_t));
-    memcpy(&new_u_tx->block_hash, block_hash, SHA256_DIGEST_LENGTH);
-    memcpy(&new_u_tx->tx_id, tx_id, SHA256_DIGEST_LENGTH);
-    memcpy(&new_u_tx->out, out, sizeof(tx_out_t));
-    return (new_u_tx);
+	if (!tx_id || !block_hash || !out)
+		return (NULL);
+	new_u_tx = calloc(sizeof(unspent_tx_out_t), 1);
+	memcpy(&new_u_tx->block_hash, block_hash, SHA256_DIGEST_LENGTH);
+	memcpy(&new_u_tx->tx_id, tx_id, SHA256_DIGEST_LENGTH);
+	memcpy(&new_u_tx->out, &out, sizeof(tx_out_t));
+	return (new_u_tx);
 }

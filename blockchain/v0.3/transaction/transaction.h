4d89cf67efbf7087856e539d778b59d3f83158f5
root <root@LAPTOP-FFBJSM4C>
2024-03-03 08:47:08 -0600

    header update

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -1,4 +1,8 @@
 #ifndef _TRANSACTION_H_
 #define _TRANSACTION_H_
 #include "blockchain.h"
+
+unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
+                                        uint8_t tx_id[SHA256_DIGEST_LENGTH],
+                                        tx_out_t const *out);
 #endif

355f7d1a538a52c62dbbc796257b3601947958ee
root <root@LAPTOP-FFBJSM4C>
2024-03-03 11:14:57 -0600

    hashes transaction

--- blockchain/v0.3/transaction/transaction_hash.c
+++ blockchain/v0.3/transaction/transaction_hash.c
@@ -1,5 +1,15 @@
 #include "transaction.h"
 
+/**
+ * mem_append - Appends data to a buffer
+ * @buf: Pointer to the buffer to append data to
+ * @data: Pointer to the data to append
+ * @pointer: Pointer to the current position in the buffer
+ *
+ * This function appends the specified data to the buffer at the current
+ * position indicated by the pointer. The pointer is updated to point to the
+ * next available position in the buffer after appending the data.
+ */
 static void mem_append(uint8_t *buf, uint8_t *data, size_t *pointer)
 {
 	memcpy(buf + *pointer, data, 32);

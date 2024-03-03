c9ca3d12b1bc158aa6f499f5bbb91b5baac6916c
root <root@LAPTOP-FFBJSM4C>
2024-03-03 09:55:24 -0600

    tx_in create

---
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -0,0 +1,15 @@
+#include "transaction.h"
+
+tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
+{
+	tx_in_t *new_tx_in;
+
+	if (!unspent)
+		return (NULL);
+	new_tx_in = calloc(sizeof(tx_in_t), 1);
+
+	memcpy(&new_tx_in->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
+	memcpy(&new_tx_in->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH);
+	memcpy(&new_tx_in->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH);
+	return (new_tx_in);
+}

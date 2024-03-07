7bbfebf72cb96f7d0f62ced9ad6214d1c9076452
root <root@LAPTOP-FFBJSM4C>
2024-03-07 11:47:41 -0600

    check validity of coinbase

--- blockchain/v0.3/transaction/coinbase_is_valid.c
+++ blockchain/v0.3/transaction/coinbase_is_valid.c
@@ -1,29 +1,40 @@
 #include "transaction.h"
 
-int coinbase_is_valid(transaction_t const *coinbase, uint32_t block_index)
+/**
+ * coinbase_is_valid - Checks if a coinbase transaction is valid
+ * @coinbase: Pointer to the coinbase transaction to validate
+ * @block_index: Index of the block containing the coinbase transaction
+ *
+ * Return: 1 if the coinbase transaction is valid, 0 otherwise
+ */
+
+int coinbase_is_valid(transaction_t const *coinbase,
+	uint32_t block_index)
 {
-    uint8_t hash_buf[SHA256_DIGEST_LENGTH];
-    tx_in_t *input;
-    tx_out_t *output;
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH];
+	tx_in_t *input;
+	tx_out_t *output;
 
 
-    if (memcmp(transaction_hash(coinbase, hash_buf), coinbase->id, SHA256_DIGEST_LENGTH))
-        return (0);
+	if (memcmp(transaction_hash(coinbase, hash_buf), coinbase->id,
+		SHA256_DIGEST_LENGTH))
+		return (0);
 
-    if (llist_size(coinbase->inputs) != 1 || llist_size(coinbase->outputs) != 1)
-        return (0);
-    
-    input = llist_get_node_at(coinbase->inputs , 0);
-    output = llist_get_node_at(coinbase->outputs, 0);
+	if (llist_size(coinbase->inputs) != 1 ||
+		llist_size(coinbase->outputs) != 1)
+		return (0);
+	
+	input = llist_get_node_at(coinbase->inputs , 0);
+	output = llist_get_node_at(coinbase->outputs, 0);
 
-    if (memcmp(input->tx_out_hash, &block_index, 4))
-        return (0);
-    
-    if (*input->block_hash || *input->tx_id)
-        return (0);
-    
-    if (output->amount != COINBASE_AMOUNT)
-        return (0);
-    
-    return (1);
+	if (memcmp(input->tx_out_hash, &block_index, 4))
+		return (0);
+	
+	if (*input->block_hash || *input->tx_id)
+		return (0);
+	
+	if (output->amount != COINBASE_AMOUNT)
+		return (0);
+	
+	return (1);
 }

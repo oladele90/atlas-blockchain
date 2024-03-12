486587776498944ad0ad10bab40971e7a73cd2e8
root <root@LAPTOP-FFBJSM4C>
2024-03-12 12:59:08 -0500

    update

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -1,17 +1,17 @@
 #include "blockchain.h"
 
 block_t *block_create(block_t const *prev, int8_t const *data,
-					   uint32_t data_len)
+												uint32_t data_len)
 {
+	time_t current;
 	block_t *new_block = calloc(1, sizeof(block_t));
-	uint32_t d_len = data_len > BLOCKCHAIN_DATA_MAX ?
-data_len : data_len;
 
+	current = time(NULL);
 	memcpy(new_block->data.buffer, data, data_len);
-	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-	new_block->data.len = d_len;
-	new_block->info.timestamp = syscall(SYS_time, NULL);
+	new_block->data.len = data_len;
 	new_block->info.index = prev->info.index + 1;
+	new_block->info.timestamp = current;
+	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 	new_block->transactions = llist_create(MT_SUPPORT_FALSE);
 
 	return (new_block);
